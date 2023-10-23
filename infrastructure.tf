81e048f1b48d9c61a55cbaaa92deb46dcfd33fea
Matt <matt.palermo@gmail.com>
2023-10-23 08:51:20 -0400

    save

--- infrastructure.tf
+++ infrastructure.tf
@@ -12,17 +12,13 @@ resource "aws_internet_gateway" "gw" {
 
 }
 
-resource "aws_default_route_table" "route_table" {
-  default_route_table_id = aws_vpc.default.default_route_table_id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  #  tags = {
-  #    Name = "default table"
-  #  }
-}
+# resource "aws_default_route_table" "route_table" {
+#   default_route_table_id = aws_vpc.default.default_route_table_id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+#   }
+# }
 
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.default.id
